dc39e470447275c6befb8bf1d42386754f10eddb
uopsdod <uopsdod@gmail.com>
2021-04-13 17:07:31 -0700

    first commit

---
+++ src/main/java/com/s21/sda/SdaApplication.java
@@ -0,0 +1,13 @@
+package com.s21.sda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaApplication.class, args);
+	}
+
+}
